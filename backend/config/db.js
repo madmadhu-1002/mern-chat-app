00c26bc7785545135d841b3d74cfc06cd4e468ec
Mahidhar Kosigi <mahidharkosigi@gmail.com>
2023-10-05 15:16:31 +0530

    mern-chat-app

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+
+const connectDB = async () => {
+  try {
+    console.log("MONGO_URI:", process.env.MONGO_URI);
+
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red.bold);
+    process.exit();
+  }
+};
+
+module.exports = connectDB;
